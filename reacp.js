dbebc8b04b67200ae27ded6ed19268900b6196f9
sabbir hossain <mdsabbir477470@gmail.com>
2022-08-09 11:39:22 +0600

    feet to inch explore

--- reacp.js
+++ reacp.js
@@ -1,18 +1,35 @@
-// 1. feet to inch
-feetToInch
+// 1. feet to inch convert use to function.
+function feetToInch(feetes) {
+   const inch = 12 * feetes;
+   return inch;
+}
+const feet = 12;
+const feets = feetToInch(mamaFeet);
+console.log(feets);
+
+
+// inch to feet convert use to function.
+function inchToFeet(inches) {
+   const feet = inches / 12;
+   return feet;
+}
+// dadaFeets
+const dadaInche = 144;
+const dadaFeets = inchToFeet(dadaInche);
+console.log(dadaFeets);
 
 // 2. centimeter to meter
-centimeterToMeter
+// centimeterToMeter
 
 // 3. count paper
 /* book1 ---> 100;
    book2 ---> 200;
    book3 ---> 300;
 */
-paperRequirements
+// paperRequirements
 
 // 4. ['sajid', 'mamun', 'kamal', 'jubayer bin rased', 'Chinku'];
-bestFriend
+// bestFriend
 
 // 5. [45, 87, 96, 11, 63, -56, 71, 28];
-onlyPositive
+// onlyPositive
