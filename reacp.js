12fa9581cd4e4b0f777375e04b691c4d4e3041a9
sabbir hossain <mdsabbir477470@gmail.com>
2022-08-10 19:44:51 +0600

    2 problem akon oo ses hoy nai

--- reacp.js
+++ reacp.js
@@ -189,10 +189,10 @@ console.log(myArray); // done
 
 // ৯. তুমি কি একটা ফর লুপ দিয়ে কোন একটা array এর সবগুলা উপাদানকে দেখাতে পারবে। সেটা রেগুলার for লুপ হোক বা for of হোক। হলে সেই স্টাইলে একটা কোড লিখে ফেলো।
 
-// let totalArray = [40, 50, 60, 70, 80, 90, 30, 20, 10];
-// for (let i = 0; i < i.length; i++) {
-//    console.log('Our total array defind of for loop: ', totalArray[i]);
-// }
+const array = [40, 10, 10, 10, 10, 10, 10, 10, 10];
+for (let i = 0; i < i.length; i++) {
+   console.log(array[i]);
+}
 
 // ১০. তোমাকে যদি বলা হয়। একটা array এর মধ্যে ৮০ এর চাইতে বড় সংখ্যা থাকলে সেগুলাকে console log করে দেখাতে সেটা কি তুমি পারবে? তাহলে তুমি সেই কোড করে ফেলো
 
